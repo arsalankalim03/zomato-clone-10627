052ba649f55cec7f1c0aae22669c8a4c76567dce
arsalankalim03 <arsalankalim@gmail.com>
2024-09-18 15:34:24 +0530

    completed middleware for user authentication

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+
+// Private route authorization config
+import privateRouteConfig from "./config/router.config";
 
 // Database connection
 import ConnectDB from "./database/connection";
 
 import Auth from "./database/api/auth";
+import Food from "./database/api/food";
+import Restaurant from "./database/api/restaurant";
+import User from "./database/api/user";
 
 dotenv.config();
 
+privateRouteConfig(passport);
+
 const zomato = express();
 
+// adding additional passport configuration
+
 zomato.use(express.json());
+zomato.use(session({ secret: "ZomatoApp" }));
+zomato.use(passport.initialize());
+zomato.use(passport.session());
 
 zomato.get("/", (req, res) => {
     res.json({
@@ -20,6 +35,9 @@ zomato.get("/", (req, res) => {
 
 // /auth/signup
 zomato.use("/auth", Auth);
+zomato.use("/food", Food);
+zomato.use("/restaurant", Restaurant);
+zomato.use("/user", passport.authenticate("jwt", { session: false }), User);
 
 const PORT = 4000;
 

78a43625673106445b65f698b826648cf349b8a3
arsalankalim03 <arsalankalim@gmail.com>
2024-09-20 00:39:14 +0530

    added validation to some routes

--- server/database/api/auth/index.js
+++ server/database/api/auth/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 
 import { UserModel } from "../../allModels";
+import {
+    ValidateSignin,
+    ValidateSignup,
+} from "../../../validation/auth.validation";
 const Router = express.Router();
 
 Router.post("/signup", async (req, res) => {
     try {
+        await ValidateSignup(req.body.credentials);
         await UserModel.findByEmailAndPhone(req.body.credentials);
-
         const newUser = await UserModel.create(req.body.credentials);
         const token = newUser.generateJwtToken();
         return res.status(200).json({ token, status: "success" });
@@ -17,6 +21,7 @@ Router.post("/signup", async (req, res) => {
 
 Router.post("/signin", async (req, res) => {
     try {
+        await ValidateSignin(req.body.credentials);
         const user = await UserModel.findByEmailAndPassword(req.body.credentials);
         const token = user.generateJwtToken();
         return res.status(200).json({ token, status: "success" });

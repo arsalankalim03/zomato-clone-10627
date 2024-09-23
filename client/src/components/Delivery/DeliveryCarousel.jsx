19d4b416c34816bc2e541677ab7db5eab96f578e
arsalankalim03 <arsalankalim@gmail.com>
2024-09-23 23:06:58 +0530

    completed restaurant cards and dining page

--- client/src/components/Delivery/DeliveryCarousel.jsx
+++ client/src/components/Delivery/DeliveryCarousel.jsx
@@ -86,15 +86,15 @@ const DeliveryCarousel = () => {
                 Inspiration for yout first order
             </h1>
             <div className="lg:hidden grid grid-cols-3 md:grid-cols-4 gap-3 justify-center">
-                {categories.map((food) => (
-                    <DeliveryCategoryCard {...food} />
+                {categories.map((food, index) => (
+                    <DeliveryCategoryCard key={index} {...food} />
                 ))}
             </div>
             <div className="hidden lg:block">
                 <Swiper {...slideConfig}>
-                    {categories.map((food) => (
+                    {categories.map((food, index) => (
                         <SwiperSlide>
-                            <DeliveryCategoryCard {...food} />
+                            <DeliveryCategoryCard key={index} {...food} />
                         </SwiperSlide>
                     ))}
                 </Swiper>
